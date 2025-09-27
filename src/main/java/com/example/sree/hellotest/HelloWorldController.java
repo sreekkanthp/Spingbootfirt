3e15f9bdc99807ff807bd1482f37c681ee1a402a
sreekanth <sreekkantp@gmail.com>
2025-09-27 10:07:51 +0530

    This is my second commit

--- src/main/java/com/example/sree/hellotest/HelloWorldController.java
+++ src/main/java/com/example/sree/hellotest/HelloWorldController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
 @GetMapping("/")
     public String sayHello() {
-        return "Hello, World!";
+        return "Hello, World! this is my second commit";
     }
 
 }
