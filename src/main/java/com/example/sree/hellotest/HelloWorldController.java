3b10de9f1ea990f8d41dea21a1e20c44ead1c829
sreekanth <sreekkantp@gmail.com>
2025-09-27 09:32:34 +0530

    Initial commit

---
+++ src/main/java/com/example/sree/hellotest/HelloWorldController.java
@@ -0,0 +1,11 @@
+package com.example.sree.hellotest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class HelloWorldController {
+@GetMapping("/")
+    public String sayHello() {
+        return "Hello, World!";
+    }
+
+}
